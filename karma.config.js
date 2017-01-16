d3f5a614e100e4a455fe7f2ec9a4c55d0d147899
PRIJCK Frederik (FPRJ) <frederik.prijck@gmail.com>
2017-01-16 16:08:08 +0100

    feat: add basic karma infrastructure
    Closes: #1

---
+++ karma.config.js
@@ -0,0 +1,33 @@
+
+module.exports = function(config) {
+  config.set({
+
+    browsers: [
+      'PhantomJS'
+    ],
+
+    frameworks: [
+      'jasmine'
+    ],
+
+    files: [
+      'spec.bundle.js'
+    ],
+
+    preprocessors: {
+      'spec.bundle.js': ['webpack']
+    },
+
+    webpack: require('./webpack-test.config'),
+
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
+
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-phantomjs-launcher'),
+      require('karma-webpack')
+    ]
+  });
+};
